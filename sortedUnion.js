5b67e919187531e512a77db38fab8803dc8845c6
Octafin <leoandrebenezit@gmail.com>
2017-06-12 13:07:11 +0100

    Create sortedUnion.js
    Write a function that takes two or more arrays and returns a new array of unique values in the order of the original provided arrays.
    
    In other words, all values present from all arrays should be included in their original order, but with no duplicates in the final array.
    
    The unique numbers should be sorted by their original order, but the final array should not be sorted in numerical order.

---
+++ sortedUnion.js
@@ -0,0 +1,25 @@
+function uniteUnique() {
+  
+   var newArr = [];
+  
+    for (var i = 0; i < arguments.length; i++) {
+      for (var y = 0; y < arguments[i].length; y++) {
+        if (newArr.indexOf(arguments[i][y]) < 0) {
+          
+          newArr.push(arguments[i][y]);
+        } else {
+          
+          continue;
+          
+        }
+    
+    
+    
+    }
+  }
+  
+  return newArr;
+ 
+}
+
+uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
